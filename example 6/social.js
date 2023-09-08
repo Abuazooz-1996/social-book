cb580cd779bcdd45c3b01f48cca8a95efb45e822
Abuazooz-1996 <117088151+Abuazooz-1996@users.noreply.github.com>
2023-09-08 17:55:30 +0300

    Add files via upload

---
+++ example 6/social.js
@@ -0,0 +1,34 @@
+let set=document.querySelector(".settings-menu")
+let darkbtn=document.querySelector("#dark-btn")
+console.log(darkbtn)
+
+function sett(){
+    set.classList.toggle("setting-height")
+}
+
+darkbtn.onclick=function(){
+    darkbtn.classList.toggle("dark-btn-on")
+    document.body.classList.toggle("dark-theme")
+    if(localStorage.getItem("theme") =="light"){
+        localStorage.setItem("theme","dark" )
+    }
+    else{
+        localStorage.setItem("theme","light" )
+    }
+    
+    
+}
+
+if(localStorage.getItem("theme")== "light" ){
+    darkbtn.classList.remove("dark-btn-on")
+    document.body.classList.remove("dark-theme")
+}
+else if(localStorage.getItem("theme")== "dark" ){
+    darkbtn.classList.add("dark-btn-on")
+    document.body.classList.add("dark-theme")
+}
+
+else{
+    localStorage.setItem("theme","light")
+    
+}
